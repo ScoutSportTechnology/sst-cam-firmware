366048117e99e5f3f228cd096e32234e6ca218d8
Raul Santamaria <raulsantamariao@hotmail.com>
2025-07-29 00:21:46 -0500

    Refactor track method to handle detection data conditionally and update frames processing in frames method

--- src/app/services/video/video.py
+++ src/app/services/video/video.py
@@ -55,23 +55,27 @@ class VideoService:
 		self.frame = processor.VideoPostProcessorService().process(self.frame0, self.frame1)
 		return self.frame
 
-	def track(self, detection_data_0: DetectionData, detection_data_1: DetectionData) ->  None:
-		motion_data_0: MotionData = self.motion_service.calculate_motion(detection_data_0)
-		motion_data_1: MotionData = self.motion_service.calculate_motion(detection_data_1)
+	def track(self) ->  None:
+		detection_data_0: DetectionData | None = None # i havent done this yet
+		detection_data_1: DetectionData | None = None
+		
+		if detection_data_0 is not None and detection_data_1 is not None:
+			motion_data_0: MotionData = self.motion_service.calculate_motion(detection_data_0)
+			motion_data_1: MotionData = self.motion_service.calculate_motion(detection_data_1)
 
-		side: FrameSideDecision = self.side_decider.decide_side(
-			motion_data_0, motion_data_1
-		)
-		if side == FrameSideDecision.side.LEFT:
-			self.motion_data: MotionData = motion_data_0
-			self.detection_data: DetectionData = detection_data_0
-			self.frame = self.frame0
-		else:
-			self.motion_data: MotionData = motion_data_1
-			self.detection_data: DetectionData = detection_data_1
-			self.frame = self.frame1
+			side: FrameSideDecision = self.side_decider.decide_side(
+				motion_data_0, motion_data_1
+			)
+			if side == FrameSideDecision.side.LEFT:
+				self.motion_data: MotionData = motion_data_0
+				self.detection_data: DetectionData = detection_data_0
+				self.frame = self.frame0
+			else:
+				self.motion_data: MotionData = motion_data_1
+				self.detection_data: DetectionData = detection_data_1
+				self.frame = self.frame1
 
-		self.zoom_data: ZoomData = self.zoom_service.calculate_zoom(self.detection_data, self.motion_data)
+			self.zoom_data: ZoomData = self.zoom_service.calculate_zoom(self.detection_data, self.motion_data)
 
 	def transform(self) -> None:
 		self.frame = processor.VideoTransformationService().process(self.frame, self.zoom_data)
@@ -86,13 +90,13 @@ class VideoService:
 		stream_service.start()
 		return stream_service
 
-def frames(self) -> Generator[Frame, None, None]:
-	while self.active:
-		self.get_frames()
-		self.calibrate()
-		self.preprocess()
-		self.track()
-		self.transform()
-		self.overlay()
-		self.postprocess()
-		yield self.frame
+	def frames(self) -> Generator[Frame, None, None]:
+		while self.active:
+			self.get_frames()
+			#self.calibrate()
+			self.preprocess()
+			#self.track()
+			#self.transform()
+			#self.overlay()
+			self.postprocess()
+			yield self.frame
